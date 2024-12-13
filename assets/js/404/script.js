83ce063c946982d377cce2d69513623fa84c06f9
Danial Alidaryan <alidaryandanial@gmail.com>
2024-12-13 15:09:17 +0330

    add 404

---
+++ assets/js/404/script.js
@@ -0,0 +1,9 @@
+let container = document.querySelector('#container');
+
+window.onmousemove = e => {
+    let x = e.clientX / 8;
+    let y = e.clientY / 8;
+
+    container.style.backgroundPositionX = x + "px";
+    container.style.backgroundPositionY = y + "px";
+}
