76eb9e34fb28f306dde64f2c58172ccb331c944f
Danial Alidaryan <alidaryandanial@gmail.com>
2024-12-13 14:32:19 +0330

    add login page view, modifie url.py and views.py

--- Amoozeshyar/urls.py
+++ Amoozeshyar/urls.py
@@ -8,8 +8,8 @@ Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    1. Add an import:  from other_app.views import general
+    2. Add a URL to urlpatterns:  path('', general.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
@@ -21,5 +21,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("", include('Home.urls'))
+    path("", include('general.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- general/urls.py
+++ general/urls.py
@@ -1,18 +1,22 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import (FacultyViewSet, ProfessorViewSet, StudentViewSet,
-                   CourseViewSet, SemesterViewSet, ClassScheduleViewSet,
-                   EnrollmentViewSet)
-
-router = DefaultRouter()
-router.register(r'faculties', FacultyViewSet)
-router.register(r'professors', ProfessorViewSet)
-router.register(r'students', StudentViewSet)
-router.register(r'courses', CourseViewSet)
-router.register(r'semesters', SemesterViewSet)
-router.register(r'class-schedules', ClassScheduleViewSet)
-router.register(r'enrollments', EnrollmentViewSet)
-
+from . import views
+# from rest_framework.routers import DefaultRouter
+# from .views import (FacultyViewSet, ProfessorViewSet, StudentViewSet,
+#                    CourseViewSet, SemesterViewSet, ClassScheduleViewSet,
+#                    EnrollmentViewSet)
+#
+# router = DefaultRouter()
+# router.register(r'faculties', FacultyViewSet)
+# router.register(r'professors', ProfessorViewSet)
+# router.register(r'students', StudentViewSet)
+# router.register(r'courses', CourseViewSet)
+# router.register(r'semesters', SemesterViewSet)
+# router.register(r'class-schedules', ClassScheduleViewSet)
+# router.register(r'enrollments', EnrollmentViewSet)
+app_name = "general"
 urlpatterns = [
-    path('api/', include(router.urls)),
-] 
+    # path('api/', include(router.urls)),
+    path("", views.homePage, name="home"),
+    path("contactUs", views.contactUS, name="contactUs"),
+    path("login", views.login, name="login")
+]
